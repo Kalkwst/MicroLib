55fa39cef8d27ffca46dedcfa73dc7774b50f01b
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-01-04 20:17:53 +0200

    :sparkles: Add Tokenizer interface
    The tokenizer interface provides a single method that provides a set of tokens generated from a single string. 
    
    These tokens can vary based on the concrete implementation of the interface. Example tokens can include words, characters or substrings.
    
    At the time of writing this interface only has one implementation in the library. This interface is primarily designed to be an extension point for different tokenizers that can be used with similarity calculators provided by this library.

---
+++ Text/Similarity/Tokenizers/ITokenizer.cs
@@ -0,0 +1,15 @@
+﻿namespace Text.Similarity.Tokenizers;
+
+/// <summary>
+/// A tokenizer. Can produce arrays of tokens from a given type.
+/// </summary>
+/// <typeparam name="T">The type of tokens generated.</typeparam>
+public interface ITokenizer<T>
+{
+    /// <summary>
+    /// Returns an array of tokens.
+    /// </summary>
+    /// <param name="text">The input text.</param>
+    /// <returns>An array of tokens.</returns>
+    public T[] Tokenize(string text);
+}
