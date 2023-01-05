8bbb585da286eb9c07c2696a7b28b8fbe9b5361b
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-01-05 15:26:15 +0200

    :white_check_mark: Add tests for Cosine Similarity

---
+++ TextTests/Similarity/Cosine/CosineSimilarityTests.cs
@@ -0,0 +1,28 @@
+﻿using Text.Similarity.Cosine;
+
+namespace TextTests.Similarity.Cosine;
+
+[TestFixture]
+public class CosineSimilarityTests
+{
+    [Test]
+    public void CosineSimilarityReturnsDoubleWhereByteValueIsZero()
+    {
+        var cosineSimilarity = new CosineSimilarity();
+
+        var result = cosineSimilarity.Calculate("", "");
+
+        Assert.That(result, Is.EqualTo(0.0).Within(0.01));
+    }
+
+    [Test]
+    public void CosineSimilarityThrowsArgumentNullExceptionForNullDictionary()
+    {
+        var cosineSimilarity = new CosineSimilarity();
+        
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            cosineSimilarity.Calculate("string", null);
+        });
+    }
+}
