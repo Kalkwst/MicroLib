4d1175c5ed5bdff1397ac4b87c491cc8531be17c
Kalkwst <kalafatiskwstas@gmail.com>
2023-01-05 16:10:30 +0200

    :sparkles: Text: Add Hamming Distance Calculation Algorithm (#3)
    * :sparkles: Add Hamming Distance calculation algorithm
    
    * :white_check_mark: Add tests for Hamming Distance calculation

---
+++ Text/Similarity/Hamming/HammingDistance.cs
@@ -0,0 +1,36 @@
+﻿namespace Text.Similarity.Hamming;
+
+/// <summary>
+/// The hamming distance between two strings of equal length is the number of positions at which the corresponding
+/// symbols are different.
+/// </summary>
+public class HammingDistance : IEditDistance<int>
+{
+    /// <summary>
+    /// Find the Hamming Distance between two strings with the same length.
+    /// <br/><br/>
+    /// The distance starts with zero, and for each occurrence of a different character in either string, it increments
+    /// the distance by 1, and finally return its value.
+    /// <br/><br/>
+    /// Since the Hamming Distance can only be calculated between strings of equal length, input of different lengths
+    /// will throw an <see cref="ArgumentException"/>.
+    /// </summary>
+    /// <param name="left">The first string, must not be null.</param>
+    /// <param name="right">The second string, must not be null.</param>
+    /// <returns>The number of differences between the two strings</returns>
+    /// <exception cref="ArgumentNullException">If any of the two strings is null.</exception>
+    /// <exception cref="ArgumentException">If the strings do not have the same length.</exception>
+    public int Calculate(string left, string right)
+    {
+        if (left == null || right == null)
+            throw new ArgumentNullException(left == null ? nameof(left) : nameof(right), "Strings must not be null");
+
+        if (left.Length != right.Length)
+            throw new ArgumentException("Strings must have the same length");
+
+        left = left.ToLower();
+        right = right.ToLower();
+
+        return left.Where((t, i) => t != right[i]).Count();
+    }
+}
---
+++ TextTests/Similarity/Hamming/HammingDistanceTests.cs
@@ -0,0 +1,55 @@
+﻿using Text.Similarity.Hamming;
+
+namespace TextTests.Similarity.Hamming;
+
+[TestFixture]
+public class HammingDistanceTests
+{
+    private HammingDistance distance;
+
+    [SetUp]
+    public void Init()
+    {
+        distance = new HammingDistance();
+    }
+
+    [TestCase("", "", 0)]
+    [TestCase("pappa", "pappa", 0)]
+    [TestCase("papaa", "pappa", 1)]
+    [TestCase("karolin", "kathrin", 3)]
+    [TestCase("karolin", "kerstin", 3)]
+    [TestCase("1011101", "1001001", 2)]
+    [TestCase("2173896", "2233796", 3)]
+    [TestCase("ATCG", "ACCC", 2)]
+    public void HammingDistanceValidStrings(string leftValue, string rightValue, int expectedDistance)
+    {
+        // Arrange
+        // The distance object is initialized in the SetUp method
+
+        // Act
+        int actualDistance = distance.Calculate(leftValue, rightValue);
+
+        // Assert
+        Assert.That(actualDistance, Is.EqualTo(expectedDistance));
+    }
+
+    [Test]
+    public void HammingDistanceNullLeftValue()
+    {
+        // Arrange
+        // The distance object is initialized in the SetUp method
+
+        // Act and Assert
+        Assert.Throws<ArgumentNullException>(() => distance.Calculate(null, ""));
+    }
+
+    [Test]
+    public void HammingDistanceNullRightValue()
+    {
+        // Arrange
+        // The distance object is initialized in the SetUp method
+
+        // Act and Assert
+        Assert.Throws<ArgumentNullException>(() => distance.Calculate("", null));
+    }
+}
