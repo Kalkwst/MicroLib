75356dc6180a0eb0c27e72f4c6bb17276297e1f3
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-01-18 13:54:21 +0200

    :sparkles: Add NotNullPredicate

---
+++ Collections/Functors/Predicates/NotNullPredicate.cs
@@ -0,0 +1,18 @@
+﻿namespace Collections.Functors.Predicates;
+
+/// <summary>
+/// Predicate implementation that returns true if the input is not null.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class NotNullPredicate<T> : IPredicate<T>
+{
+    /// <summary>
+    /// Determines that the value provided to this <see cref="IPredicate{T}"/> is not null.
+    /// </summary>
+    /// <param name="element">The element to evaluate.</param>
+    /// <returns>true if the element is not null, false otherwise.</returns>
+    public bool Evaluate(T element)
+    {
+        return element != null;
+    }
+}
