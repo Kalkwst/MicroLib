2b09413ebe28be6256c59006c749fd8ddad68df5
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-01-18 13:13:43 +0200

    :sparkles: Add IPredicateDecorator

---
+++ Collections/Functors/Predicates/IPredicateDecorator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+
+namespace Collections.Functors.Predicates;
+
+/// <summary>
+/// Defines an <see cref="IPredicate{T}"/> that decorates one or more <see cref="IPredicate{T}"/>s.
+/// </summary>
+/// <typeparam name="T">The type parameter of the predicates.</typeparam>
+public interface IPredicateDecorator<T> : IPredicate<T>
+{
+    /// <summary>
+    /// Gets the predicates being decorated as an array.
+    /// </summary>
+    /// <returns>The predicates being decorated.</returns>
+    public ImmutableArray<IPredicate<T>> GetPredicates();
+}
