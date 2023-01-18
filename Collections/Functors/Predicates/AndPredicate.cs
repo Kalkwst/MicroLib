09d894d3710a1d86d69b96eb2736505ea736651b
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-01-18 13:13:29 +0200

    :sparkles: Add AndPredicate

---
+++ Collections/Functors/Predicates/AndPredicate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+
+namespace Collections.Functors.Predicates;
+
+/// <summary>
+/// Predicate implementation that returns true if both the predicates return true.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class AndPredicate<T> : IPredicateDecorator<T>
+{
+    /// <summary>
+    /// The first predicate to call.
+    /// </summary>
+    private readonly IPredicate<T> _firstPredicate;
+
+    /// <summary>
+    /// The second predicate to call.
+    /// </summary>
+    private readonly IPredicate<T> _secondPredicate;
+
+    public AndPredicate(IPredicate<T> firstPredicate, IPredicate<T> secondPredicate)
+    {
+        _firstPredicate = firstPredicate;
+        _secondPredicate = secondPredicate;
+    }
+
+    /// <summary>
+    /// Evaluates that both predicates return true for the given input.
+    /// </summary>
+    /// <param name="element">The element to evaluate.</param>
+    /// <returns>true if both decorated predicates return true, false otherwise.</returns>
+    public bool Evaluate(T element)
+    {
+        return _firstPredicate.Evaluate(element) && _secondPredicate.Evaluate(element);
+    }
+
+    ///<inheritdoc cref="IPredicateDecorator{T}.GetPredicates"/>
+    public ImmutableArray<IPredicate<T>> GetPredicates()
+    {
+        return new ImmutableArray<IPredicate<T>>() { _firstPredicate, _secondPredicate };
+    }
+}
