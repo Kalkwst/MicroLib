00f65e1ed5d9b311356dd14547ea5645eca78505
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-01-20 11:38:09 +0200

    :sparkles: Add NullPredicate

---
+++ Collections/Functors/Predicates/NullPredicate.cs
@@ -0,0 +1,18 @@
+﻿namespace Collections.Functors.Predicates;
+
+/// <summary>
+/// Predicate implementation that returns true if the input is null.
+/// </summary>
+/// <typeparam name="T">The type parameter of the predicate.</typeparam>
+public class NullPredicate<T> : IPredicate<T>
+{
+    /// <summary>
+    /// Determines whether the provided input is null.
+    /// </summary>
+    /// <param name="element">The element to determine if its null.</param>
+    /// <returns>true if the element is null, false otherwise.</returns>
+    public bool Evaluate(T element)
+    {
+        return element == null;
+    }
+}
