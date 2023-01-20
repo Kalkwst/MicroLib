f84de4676f65c7d620bd7068dee44b7ef3d2e360
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-01-20 14:52:16 +0200

    :white_check_mark: Add TypeOfPredicate tests

---
+++ CollectionsTests/Functors/Predicates/TypeOfPredicateTests.cs
@@ -0,0 +1,26 @@
+﻿using Collections.Functors.Predicates;
+
+namespace CollectionsTests.Functors.Predicates;
+
+[TestFixture]
+public class TypeOfPredicateTests
+{
+
+    [Test]
+    public void TypeOfPredicate_ForTheSameType_ReturnsTrue()
+    {
+        var type = typeof(int);
+        var predicate = new TypeOfPredicate<object>(type);
+        
+        Assert.That(predicate.Evaluate(32), Is.True);
+    }
+
+    [Test]
+    public void TypeOfPredicate_ForDifferentType_ReturnsFalse()
+    {
+        var type = typeof(int);
+        var predicate = new TypeOfPredicate<object>(type);
+        
+        Assert.That(predicate.Evaluate("32"), Is.False);
+    }
+}
